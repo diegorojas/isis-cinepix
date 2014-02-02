8c6fb5441f3c7eb899c0deeeaa9f1ed04a0a83b2
Diego Rojas <diego@brasa.art.br>
2014-02-01 23:59:31 -0200

    subindido o tema atual na cinepix

---
+++ category.php
@@ -0,0 +1,20 @@
+
+<?php 
+/**
+ * The template for displaying Category pages
+ * Imonthemes
+ */
+
+
+get_header(); ?>
+<div class="row">
+<div class="large-12 ">
+ 
+<div id="content" >
+<?php get_template_part(''.$isis = of_get_option('layout1_images').''); ?>
+
+</div>
+</div>
+</div>
+
+<?php get_footer(); ?>
---
+++ index.php
@@ -0,0 +1,230 @@
+<?php
+
+
+
+ get_header(); ?>
+ <?php if ( is_home() ) { ?>
+
+<div id="slider">
+<div class="row"> 
+<div class=" columns">
+<?php if( get_option( 'isis' )){ ?>
+<?php get_template_part(''.$slides = of_get_option('slider_select', 'nivo').''); ?>
+ <?php }else{ ?>
+ 
+ <?php get_template_part('dummy/dummy','nivo'); ?>
+        <?php } ?>  
+ <?php } ?>  		
+            
+</div>
+
+
+
+</div>
+</div></div>
+
+
+
+<!--MIDROW STARTS-->
+<div class="row "> 
+
+<?php if ( is_home() ) { ?>
+
+
+
+<div class="services-wrap large-12 columns">
+
+ <?php if( get_option( 'isis' )){ ?>   
+    
+<?php if(of_get_option('blocks_checkbox','isis') == "1"){ ?>
+<div class="midrow">
+
+ <div class="midrow_wrap">       
+        <div class="midrow_blocks">  
+       
+        <div class="midrow_blocks_wrap">
+         <i class="fa <?php echo of_get_option('block1_logo'); ?> fa-3x icon"></i> 
+        <?php if ( of_get_option('block1_textarea') ) { ?>
+        <div class="midrow_block">
+        <div class="mid_block_content">
+        <h3><?php echo of_get_option('block1_text'); ?></h3>
+        <p><?php echo of_get_option('block1_textarea'); ?></p>
+    
+        </div>
+         <?php if ( of_get_option('block1_link') ) { ?><a href="<?php echo of_get_option('block1_link'); ?>" class="blocklink"><?php _e('More', 'isis'); ?></a><?php } ?>
+        </div></div>
+         <div class="shadow"><img  src="<?php echo get_template_directory_uri(); ?>/images/service_shadow.png" alt="<?php the_title_attribute(); ?>" /></div>
+       
+        </div>
+        
+         
+        <?php } ?>
+            
+        <div class="midrow_blocks">  
+        
+       <div class="midrow_blocks_wrap">
+        <i class="fa <?php echo of_get_option('block2_logo'); ?> fa-3x icon"></i>
+        <?php if ( of_get_option('block2_textarea') ) { ?>
+        <div class="midrow_block">
+        
+        <div class="mid_block_content">
+        <h3><?php echo of_get_option('block2_text'); ?></h3>
+        <p><?php echo of_get_option('block2_textarea'); ?></p>
+     
+        </div>
+        <?php if ( of_get_option('block2_link') ) { ?><a href="<?php echo of_get_option('block2_link'); ?>" class="blocklink"><?php _e('More', 'isis'); ?></a><?php } ?>
+        </div></div>
+        <div class="shadow"><img  src="<?php echo get_template_directory_uri(); ?>/images/service_shadow.png" alt="<?php the_title_attribute(); ?>" /></div>
+        
+        </div>
+        <?php } ?>
+        
+        
+         <div class="midrow_blocks">  
+        
+       <div class="midrow_blocks_wrap">
+        <i class="fa <?php echo of_get_option('block3_logo'); ?> fa-3x icon"></i>
+        
+        <?php if ( of_get_option('block3_textarea') ) { ?>
+        <div class="midrow_block">
+        
+        <div class="mid_block_content">
+        <h3><?php echo of_get_option('block3_text'); ?></h3>
+        <p><?php echo of_get_option('block3_textarea'); ?></p>
+    
+        </div>
+         <?php if ( of_get_option('block3_link') ) { ?><a href="<?php echo of_get_option('block3_link'); ?>" class="blocklink"><?php _e('More', 'isis'); ?></a><?php } ?>
+        </div></div>
+        <div class="shadow"><img  src="<?php echo get_template_directory_uri(); ?>/images/service_shadow.png" alt="<?php the_title_attribute(); ?>" /></div>
+        
+        </div>
+         <?php } ?>
+         
+         
+         <div class="midrow_blocks">  
+        
+       <div class="midrow_blocks_wrap">
+        <i class="fa <?php echo of_get_option('block4_logo'); ?> fa-3x icon"></i>
+         
+        <?php if ( of_get_option('block4_textarea') ) { ?>
+        
+        
+         
+        <div class="mid_block_content">
+        
+        <h3><?php echo of_get_option('block4_text'); ?></h3>
+        <p><?php echo of_get_option('block4_textarea'); ?></p>
+     
+        </div>
+        <?php if ( of_get_option('block4_link') ) { ?><a href="<?php echo of_get_option('block4_link'); ?>" class="blocklink"><?php _e('More', 'isis'); ?></a><?php } ?>
+        </div>
+        <div class="shadow"><img  src="<?php echo get_template_directory_uri(); ?>/images/service_shadow.png" alt="<?php the_title_attribute(); ?>" /></div>
+        <?php } ?>
+        
+        
+<?php } ?>
+
+
+<?php }else{ ?>
+ 
+ <?php get_template_part('dummy/dummy','bloks'); ?>
+        <?php } ?>  
+         
+        </div>
+        
+        
+        </div>
+     </div>   
+</div>
+ 
+        
+   
+
+
+
+<!--MIDROW END-->
+  
+        
+             
+          
+     
+
+   
+<?php }?>
+</div> 
+<?php if ( is_home() ) { ?>
+<div class="row "> 
+
+<div class="warp large-12 columns">
+<?php if( get_option( 'isis' )){ ?>
+<!--recent work-->
+<?php if(of_get_option('recentwork_checkbox','isis') == "1"){ ?>
+
+
+<?php get_template_part('parts/mid','contant'); ?>
+
+
+
+<?php }?>
+<?php }else{ ?>
+ 
+ <?php get_template_part('dummy/dummy','contant'); ?>
+        <?php } ?> 
+</div></div>		
+<?php }?></div>
+
+
+
+
+<!--recent work end-->
+<!--MIDROW END-->
+<?php if ( is_home() ) { ?>
+
+<div class="  warp row">
+<div class=" large-12">
+<section id="callout">
+<?php if ( of_get_option('isis_welcome') ) : ?>
+            
+            <!-- Start Callout section -->
+            
+            <?php echo apply_filters('the_content', of_get_option('isis_welcome')); ?>
+ </section>
+  </div></div>          <!-- END #callout -->
+
+            <?php endif; ?>
+            <?php } ?> 
+<!--Layout--></div></div>
+
+<!--LATEST POSTS-->
+<?php if( get_option( 'isis' )){ ?>
+<?php if ( is_home() ) { ?>
+<div class="row "> 
+<div class="warp large-12">
+
+	<?php if(of_get_option('latstpst_checkbox') == "1")
+	{ ?><?php get_template_part(''.$os_lays = of_get_option('layout1_images','layout1').''); ?><?php } else { ?><?php } ?>
+<?php } else { ?>
+	<?php get_template_part(''.$os_lays = of_get_option('layout1_images', 'layout1').''); ?>
+    
+<?php } ?> 
+<?php }else{ ?>
+ 
+ <?php get_template_part('dummy/dummy','layout1'); ?>
+        <?php } ?> 
+</div>
+</div>
+</div>
+ <div class="row "> 
+
+<div class=" warp large-12 columns">
+
+<?php get_template_part('parts/our','client'); ?>
+
+</div></div>
+      
+<!--LATEST POSTS END-->
+
+<!--Layout END-->
+
+
+<?php get_footer(); ?>
